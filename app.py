f3a06c861d876bc9f6f6f6f64cd567382e2faf84
Vinicius Cerqueira Oliveira <dev.vinicius.c.oliveira@gmail.com>
2022-07-14 19:57:55 -0300

    firt commit

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+hoteis = [
+    {
+        'hotel_id' : 'alpha',
+        'nome' : 'Alpha Hotel',
+        'cidade' : 'Feira de Santana',
+        'estrela' : 4.3,
+        'diaria' : 450.15
+    },
+    {
+        'hotel_id': 'bravo',
+        'nome': 'Bravo Hotel',
+        'cidade': 'Serrinha',
+        'estrela': 3.3,
+        'diaria': 270.32
+    },
+    {
+        'hotel_id': 'charlie',
+        'nome': 'Charlie Hotel',
+        'cidade': 'Salvador',
+        'estrela': 2.7,
+        'diaria': 650.82
+    },
+    {
+        'hotel_id': 'delta',
+        'nome': 'Delta Hotel',
+        'cidade': 'Camaçari',
+        'estrela': 4.7,
+        'diaria': 300.00
+    },
+]
+
+class Hoteis(Resource):
+    # ler informações
+    def get(self):
+        return {'hoteis' : hoteis}
+
+api.add_resource(Hoteis, '/hoteis')
+
+if __name__ == '__main__':
+    app.run(debug=True)
